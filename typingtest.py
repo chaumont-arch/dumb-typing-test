535971450a58ea8a851e8781fa035270cd9f1a78
chaumont-arch <chaumont@ualberta.ca>
2021-12-14 19:03:35 -0700

    renaming functions

--- typingtest.py
+++ typingtest.py
@@ -4,6 +4,7 @@ import time
 import multiprocessing
 from os import system, name
 
+#do something about the global variables - options file?
 time_limit = 60
 print_width = 50
 
@@ -15,7 +16,7 @@ def clear():
     else:
         _ = system('clear')    
     
-def chartest(goal_char):
+def test_user_on_character(goal_char):
     input_char = ''
     while input_char != goal_char:
         input_char = msvcrt.getch().decode()
@@ -23,19 +24,17 @@ def chartest(goal_char):
     # make sure it all gets printed nice
     # and display wpm when its done
     
-def runstring(text):
-    pass_flag = True
+def test_user_on_string(text):
     clear()
     for index in range(len(text)):
-        end = min(len(text),index+print_width)
-        
-        print(text[index:end])
+        final_index = min(len(text),index+print_width)
+        print(text[index:final_index])
         goal_char = text[index]
-        chartest(goal_char) #rename functions
+        test_user_on_character(goal_char)
         clear()
-    print("END OF STRING REACHED")
+    print("END OF STRING REACHED") #is there a way to kill the sleep?
         
-def run_timed(function,*args):  
+def run_function_with_timer(function,*args):  
     p = multiprocessing.Process(target=function, name="function", args=(*args,)) #messy *args handling
     p.start()   
     time.sleep(time_limit) 
@@ -44,7 +43,7 @@ def run_timed(function,*args):
         
 if __name__ == '__main__':
     message = "That way, Python will recognise it as a tuple with a single element, as intended. Currently, Python is interpreting your code as just a string. However, it's failing in this particular way because a string is effectively list of characters."
-    run_timed(runstring,message)
+    run_function_with_timer(test_user_on_string,message)
 
 #Plan:
 #3 strikes out
